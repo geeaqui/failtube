a4caecec718d2cdc8a940dede393fc186269dc59
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-08 14:25:09 +0000

    installed packages for ejs and ejs-layouts and created files for the views

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 var router = require('./config/routes');
+var layouts = require('express-ejs-layouts');
 
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
+app.use(layouts);
 app.use(router);
 
 app.listen(port, function(){
