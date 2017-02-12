93ce7ba4d24d3a297da36085a2676bb8bb9e1f33
Gerald Aqui <gaqui@spartaglobal.co>
2017-02-12 18:16:07 +0000

    created a database using require mongoose

--- controllers/videos.js
+++ controllers/videos.js
@@ -1,15 +1,40 @@
-var videos = require('../models/video');
+var Videos = require('../models/video');
 
 function indexVideos(req, res){
-	res.status(200).render('videos/index', { videos : videos });
+	// res.status(200).render('videos/index', { videos : videos });
+
+	//get the model to load all the post for me
+	Videos.find({},function(err, videos){
+		//check for errors and return a 500 error if there is a problem
+    	if(err) return res.status(500).send(err.message);
+
+    	//data return so now we can render
+	    res.render("videos/index" , {
+	      videos: videos
+	    });
+	}); 
 }
 
 function showVideos(req, res){
-	res.status(200).render('videos/show',{video: videos[req.params.id]});
+	//res.status(200).render('videos/show',{video: videos[req.params.id]});
+	Videos.findById(req.params.id, function(err, video){
+		if(!video) return res.status(404).send('Not Found');
+		if(err) return res.status(500).send(err);
+		res.render('videos/show',{
+			video:video
+		});
+	});
 }
 
+// EDIT - GET /:id/edit
 function editVideos(req, res){
-	res.status(200).render('videos/edit', {video:videos[req.params.id]});
+	//res.status(200).render('videos/edit', {video:videos[req.params.id]});
+	Videos.findById(req.params.id, function(err, video){
+		if(err) return res.status(500).send(err);
+		res.render('videos/edit',{
+			video: video
+		});
+	});
 }
 
 function newVideos(req, res){
@@ -28,38 +53,53 @@ function newVideos(req, res){
 }
 
 function updateVideos(req,res){
-	var video = videos[req.params.id];
+	// var video = videos[req.params.id];
 
-	video.title = req.body.title;
-	video.description = req.body.description;
-	video.url = req.body.url;
-	video.failLevel = req.body.failLevel;
-	video.nsfw = req.body.nsfw;
+	// video.title = req.body.title;
+	// video.description = req.body.description;
+	// video.url = req.body.url;
+	// video.failLevel = req.body.failLevel;
+	// video.nsfw = req.body.nsfw;
 
-	videos[req.params.id] = video;
+	// videos[req.params.id] = video;
 
-	res.redirect('/');
+	// res.redirect('/');
+
+	Videos.findByIdAndUpdate(req.params.id, req.body, function(err, post){
+    // redirect the user to a GET route. We'll go back to the INDEX.
+    res.redirect("/");
+  });
 }
 
 function createVideos(req, res){
-	var video = {
-		id: videos.length,
-		title: req.body.title,
-		description: req.body.description,
-		url: req.body.url,
-		failLevel: req.body.failLevel,
-		nsfw: req.body.nsfw
-	}
+	// var video = {
+	// 	id: videos.length,
+	// 	title: req.body.title,
+	// 	description: req.body.description,
+	// 	url: req.body.url,
+	// 	failLevel: req.body.failLevel,
+	// 	nsfw: req.body.nsfw
+	// }
 
-	videos.push(video);
+	// videos.push(video);
 
-	res.redirect('/');
+	// res.redirect('/');
+
+	// create a new post object with that data
+  
+  var video = new Videos(req.body);
+  video.save(function(err, post){
+    //handle the error
+    if(err) return re.status(500).send(err.message);
+    // redirect the user to a GET route. We'll go back to the INDEX.
+    res.redirect("/");
+  });
 }
 
 function deleteVideos(req, res){
-	videos.splice(req.params.id, 1);
-	for(var i=0; i<videos.length; i ++){
-		videos[i].id = i;
+	Videos.splice(req.params.id, 1);
+	for(var i=0; i<Videos.length; i ++){
+		Videos[i].id = i;
 	}
 	res.status(200).redirect('/');
 }
